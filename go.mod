f2ff4f657c5c3cbe76f8aa6d30dee83ab77bdb68
Brock Herion <brockheriondev@gmail.com>
2021-10-10 10:27:57 -0500

    Added in gin server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/brockherion/go-pet-clinic
 
 go 1.15
+
+require github.com/gin-gonic/gin v1.7.4
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type message struct {
+	Message string `json:"message"`
+}
+
+var pong = message{
+	Message: "pong",
+}
 
 func main() {
-	fmt.Println("Hello, pet clinic")
+	router := gin.Default()
+	router.GET("/ping", getPong)
+
+	router.Run("localhost:8080")
+}
+
+func getPong(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, pong)
 }
